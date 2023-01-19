bec45b3e26eaa8803901bf939a22d464d3e2de15
smtsuchi <shoha1999@gmail.com>
2023-01-18 21:32:18 -0500

    tuesday

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect, url_for
+from .forms import UserCreationForm
+from .models import User
 
 @app.route('/')
 def homePage():
@@ -11,3 +13,32 @@ def homePage():
 @app.route('/contact')
 def contactPage():
     return render_template('contact.html')
+
+
+@app.route('/about')
+def aboutPage():
+    return render_template('about.html')
+
+
+@app.route('/signup', methods=["GET", "POST"])
+def signUpPage():
+    form = UserCreationForm()
+    print(request.method)
+    if request.method == 'POST':
+        if form.validate():
+            username = form.username.data
+            email = form.email.data
+            password = form.password.data
+            
+            print(username, email, password)
+
+            # add user to database
+            user = User(username, email, password)
+            print(user)
+
+            user.saveToDB()
+
+            return redirect(url_for('contactPage'))
+
+
+    return render_template('signup.html', form = form )
